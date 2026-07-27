7643108d86b805e491d3d4ab46004c5b2471c3a3
AndersonGas <andersongdmf@gmail.com>
2026-07-27 20:56:08 -0300

    tratamento de dados

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -2,25 +2,49 @@ function App() {
 
   async function sincronizarDados() {
     try {
+      // Buscar usuários
       const respostaUsuarios = await fetch(
         'https://jsonplaceholder.typicode.com/users'
       );
 
       const usuarios = await respostaUsuarios.json();
 
-      console.log("Usuários:", usuarios);
+
+      // Transformar usuários para o formato do banco
+      const usuariosParaBanco = usuarios.map((usuario: any) => ({
+        id: usuario.id,
+        nome: usuario.name,
+        apelido: usuario.username,
+        empresa: usuario.company.name
+      }));
 
 
+      console.log("Usuários para o banco:", usuariosParaBanco);
+
+
+
+      // Buscar posts
       const respostaPosts = await fetch(
         'https://jsonplaceholder.typicode.com/posts'
       );
 
       const posts = await respostaPosts.json();
 
-      console.log("Posts:", posts);
+
+      // Transformar posts para o formato do banco
+      const postsParaBanco = posts.map((post: any) => ({
+        id: post.id,
+        titulo: post.title,
+        texto: post.body,
+        id_usuario: post.userId
+      }));
+
+
+      console.log("Posts para o banco:", postsParaBanco);
+
 
     } catch (erro) {
-      console.log("Erro ao buscar dados:", erro);
+      console.log("Erro na sincronização:", erro);
     }
   }
 
